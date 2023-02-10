f4f585b1ab01f774cf9e7672293977455319337e
IrfanCatovic <catovicc84@gmail.com>
2023-02-10 15:14:23 +0100

    this keyword, argument, primitives vs objects

--- Vezba i beleske/script.js
+++ Vezba i beleske/script.js
@@ -89,13 +89,77 @@ const f = Jonas.calcAge; // kopiramo funkciju
 f();
 */
 
+/*var firstName = 'Matilda';
+
 const Jonas = {
   firstName: 'Jonas',
   year: 1991,
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
+
+    //solution 1
+    /*const self = this; //self or that
+    const isMillenial = function () {
+      console.log(self); //undefind
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+    isMillenial();
+    
+
+    //solution 2
+    //Radice jer this keyword korist parent this i u ovom slucaju je to jonas
+    //ne moramo da pravimo jos jednu var self da bismo mu objasnili
+    const isMillenial = () => {
+      console.log(this); //undefind
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
-  greet: () => console.log(`hey ${this.firstName}`),
+  greet: () => {
+    console.log(this);
+    console.log(`hey ${this.firstName}`);
+  },
+  //it will be undefined bcs arrow function doesn't have his own this keyword
+  //it will use parent this keyword and that is window in this case
 };
 Jonas.greet();
+Jonas.calcAge();
+*/
+
+//ARGUMENT KEYWORD -------------------------------
+
+/*const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5); //we got array with parameters we passed in
+addExpr(2, 5, 7, 12); //we can pass more arguments but we will not see in function
+
+const addArrow = (a, b) => {
+  console.log(arguments); //doesn't work in arrow function arguments
+  return a + b;
+};*/
+
+//PRIMITIVES VS OBJECTS -------------------
+
+/*let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age); //31
+console.log(oldAge); //30 it will not effect because we changed later in code
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me);
+//we can't copy object, we just showing adress of old object to new one
+//so we bouth use same adress and when we change something on one
+//we change on bouth
+*/
