d8aa605032b55c8de5eaa85e8a9a77597607fa6a
IrfanCatovic <catovicc84@gmail.com>
2023-02-13 11:21:51 +0100

    spread vs rest operator

--- Vezba i beleske/script.js
+++ Vezba i beleske/script.js
@@ -297,6 +297,10 @@ const restaurant = {
       `Here is you delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orederPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 restaurant.orderDelivery({
@@ -385,6 +389,7 @@ console.log(restaurant.Name);
 //OD ELEMENATA PRAVI ARRAY
 //spread bcs on RIGHT side of =
 
+// 2) destructuring
 const arrRest = [1, 2, ...[3, 4]];
 
 // REST, because of LEFT side of =
@@ -396,3 +401,24 @@ const [pizza, , Risotto, ...otherFood] = [
   ...restaurant.starterMenu,
 ];
 console.log(pizza, Risotto, otherFood);
+
+//objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays); //colecting everthing ecxept sat
+
+// 2) FUNCTIONS
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const p = [23, 5, 7];
+add(...p); // kupi sve elemente iz x i salje ih u func gde ih i on kupi sa rest
+
+restaurant.orederPizza('mushrooms', 'onion', 'olives', 'spinach'); //odvaja ih jer je prvi parametar ustv parametar, a drugi je ...rest i skuplja ih u array
+restaurant.orederPizza('mushrooms'); //we will still have empty array
